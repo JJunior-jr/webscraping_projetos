7c088af0b7b41c4b1c612f9c067fa672d5f8d506
Junior <jacsonsajr.study@gmail.com>
2025-04-12 11:11:30 -0300

    criando o json

--- coleta/coleta/spiders/notbook.py
+++ coleta/coleta/spiders/notbook.py
@@ -7,4 +7,16 @@ class NotbookSpider(scrapy.Spider):
     start_urls = ["https://lista.mercadolivre.com.br/notebook#D[A:notebook]"]
 
     def parse(self, response):
+
+        products= response.css("div.ui-search-result__wrapper ") #ui-search-result__wrapper--large
+
+        for product in products:
+            yield { # o return retorna um item e o yield retorna um gerador, o que é mais eficiente
+                
+                "brand": product.css("span.poly-component__brand::text").get(),
+                "name": product.css("a.poly-component__title::text").get(),
+                #"title": produc"t.css("ui-search-result__title").get(),""
+                #"price": product.css("ui-search-result__price").get(),
+                #"link": product.css("ui-search-result__link").get(),
+            }
         pass
