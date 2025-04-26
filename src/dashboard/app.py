aa6d34e4dab2f97ba2f68ab714efe10e30d16aa2
Junior <jacsonsajr.study@gmail.com>
2025-04-26 19:15:28 -0300

    criacao do dash

---
+++ src/dashboard/app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+
+
+
+#conectar no banco de dados SQLite3
+
+conn=sqlite3.connect("data\mercadolivre.sql")
+
+#Carregar os dados da tabela "notbook" em um Dataframe pandas
+
+df= pd.read_sql_query("SELECT * FROM notbook", conn)
+
+# Fechar a conexão com a banco de dados
+
+conn.close()
+
+#Titulo da aplicação
+
+st.title("Pesquisa de Mercado - Notbooks no Mercado Livre")
+
+# Melhorar o layout com colnas para KPIs
+
+st.subheader()"KPIs Principais")
+col1, col2, col3= st.columns(3)
+
+#KPI1: Número total de itens
+
+total_itens= df.shape[0]
+col1.metric(label="Total de Notbooks", value= total_itens)
+
+#KPI2: Número de marcas únicas
+
+unique_brands= df['brand'].unique()
+col2.metric(label="Marcas Únicas", value= unique_brands)
+
+#KPI3: Preço médio novo (em reais)
+
+average_new_price= df['new_money'].mean()
+col3.metric(label= "Preço Médio (R$)", value= f"{average_new_price:.2f}")
+
+#marcas mais frequentes
+
+st.subheader("Marcas mais encontradas")
+col1, col2= st.columns([4,2])
+top_brands= df["brand"].value_counts().sort_values(ascending= False)
+col1.bar_chart(top_brands)
+col2.write(top_brands)
+
+#Preço médio por marca
+
+st.subheader("Preço Médio por Marca")
+col1, col2= st.columns([4,2])
+df_non_zero_prices= df[df['new_money']> 0]
+average_price_by_brand= df_non_zero_prices.groupby("brand")["new_money"].mean().sort_values(ascending= False)
+col1.bar_chart(average_price_by_brand)
+col2.write(average_price_by_brand)
+
+
+#satisfação média por marca
+st.subheader("Satisfação Média por Marca")
+col1, col2= st.columns([4,2])
+df_non_zero_reviews= df[df['reviews_rating_number']> 0]
+satisfaction_by_brand= df_non_zero_reviews.groupby("brand")["reviews_rating_number"].mean().sort_values(ascending= False)
+col1.bar_chart(satisfaction_by_brand)
+col2.write(satisfaction_by_brand)
+
+
+ 
+
+
+
+
+
--- src/transformLoad/main.py
+++ src/transformLoad/main.py
@@ -14,16 +14,21 @@ pd.set_option('display.max_columns', None)
 #pd.set_option.display.max_columns= None este é a versão da live
 
 #Adcionar a coluna _source com um valor fixo (neste caso é para saber de qual sistema os dados vieram, isso é importante para o ETL e colocado como padrão)
-df['_source'] = 'https://lista.mercadolivre.com.br/notebook'
+df['_source']= 'https://lista.mercadolivre.com.br/notebook'
 
 
 # adcionar a coluna _data_coleta com a data e hora atual
 
 #df['_data_coleta'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-df['_data_coleta'] = datetime.now()
+df['_data_coleta']= datetime.now()
 
-# Tratar os valores nulor para colunas numéricas e de texto(não foi feito)
+# Tratar os valores nulor para colunas numéricas e de texto(não recomendado, pois o python da algum erro com o 'None')
+
+df['old_money'] =  df['old_money'].fillna('0')
+df['new_money'] =  df['new_money'].fillna('0')
+df['reviews_rating_number'] =  df['reviews_rating_number'].fillna('0')
+df['reviews_amount'] =  df['reviews_amount'].fillna('0')
 
 
 # Garantir que estão todos como strings antes de usar o .str
@@ -44,7 +49,7 @@ df['reviews_amount'] =  df['reviews_amount'].astype(int)
 #Manter apenas produtos com preço entre 100 e 10000 reais
 
 
-df= df['_data_coleta'
+df= df[
        (df['old_money'] >= 1000) & (df['old_money'] <= 10000) &
        (df['new_money'] >= 1000) & (df['new_money'] <= 10000)
 
@@ -52,6 +57,16 @@ df= df['_data_coleta'
 
 
 print(df)
+
+# Conectar ao banco de dados SQLite (ou criar um novo se não existir)
+conn= sqlite3.connect('data/mercadolivre.sql')
+
+# salvar o Dataframe no banco de dados SQLite
+df.to_sql('notbook', conn, if_exists='replace', index= False)
+
+#Fechar conexão com o banco de dados
+conn.close() 
+
 
 
 
